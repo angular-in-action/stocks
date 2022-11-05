a40c89a0c2208b0f9867d30903732599795b07cd
Jeremy Wilken <gnomation@gnomeontherun.com>
2022-11-05 07:16:07 -0700

    dashboard component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { AppComponent } from './app.component';
 
 import { StocksService } from './services/stocks.service';
 import { SummaryComponent } from './components/summary/summary.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SummaryComponent
+    SummaryComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
